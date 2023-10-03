b0e3a2c91bacd8bd95faccc46ad60fe3cf9b6618
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-10-03 14:56:50 +0700

    .commit

--- pages/2_ 👩_👩_👦_👦_AboutMe.py
+++ pages/2_ 👩_👩_👦_👦_AboutMe.py
@@ -11,7 +11,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
+lottie_url_hello = "https://lottie.host/c6d991fa-da19-43f1-9c6b-18e5b7dc158f/Oz80uZxsmZ.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello, key="hello")
 
