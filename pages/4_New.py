98a0a90839ba2080963e25d5777dd1e61b86aa83
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-10-03 15:45:39 +0700

    ,commit

--- pages/4_New.py
+++ pages/4_New.py
@@ -8,6 +8,7 @@ st.header("Show data index price")
 df = pd.read_csv("./data/stock_index_price.csv")
 st.write(df.head(10))
 
+st.header("Show Chart index price")
 st.line_chart(
    df, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
 )
